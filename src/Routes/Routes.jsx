7ce77e9f1c9d2ca69776b5675666f7e56de8c0a0
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2023-12-21 13:05:02 +0600

    login page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import {
 } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element:<Home></Home>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
             }
         ]
     },
